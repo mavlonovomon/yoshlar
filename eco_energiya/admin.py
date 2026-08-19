ddd72b348dbb8682b4289998de824579f6fa1f08
Genius005 <Genius005@users.noreply.github.com>
2026-08-19 16:41:18 +0500

    feat: register SolarPanel in admin

--- eco_energiya/admin.py
+++ eco_energiya/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import SolarPanel
 
-# Register your models here.
+
+@admin.register(SolarPanel)
+class SolarPanelAdmin(admin.ModelAdmin):
+    list_display = ("mahalla", "is_installed", "capacity_kw", "installed_date", "updated_at")
+    list_filter = ("is_installed",)
+    search_fields = ("mahalla__name",)
