537d6bff58189ae7555ac4c02d3ee0b4a8e03b90
Genius005 <Genius005@users.noreply.github.com>
2026-08-19 16:51:45 +0500

    feat: add URL routing for eco_energiya

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,6 @@ urlpatterns = [
     path('hisobot/', include('hisobot.urls')),
     path('sorovnoma/', include('sorovnoma.urls')),
     path('ekin-yerlari/', include('ekin_yerlari.urls')),
+    path('eco-energiya/', include('eco_energiya.urls')),
     path('', include('core.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- eco_energiya/urls.py
+++ eco_energiya/urls.py
@@ -5,4 +5,6 @@ from . import views
 app_name = "eco_energiya"
 
 urlpatterns = [
+    path("", views.EcoEnergiyaListView.as_view(), name="list"),
+    path("<int:pk>/update/", views.solar_panel_update, name="update"),
 ]
